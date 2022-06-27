6356a3d4ab47e3684440c3b29901eeb771112c5b
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-06-26 17:30:44 -0700

    buttons added to offer cards

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,13 +1,16 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import './Offer.css';
 
-function Offer({ offer, user }) {
+function Offer({ offer }) {
   return (
-    <div>
+    <div className='offer'>
       <h3>Category: {offer.category}</h3>
       <h3>Description: {offer.description}</h3>
       <h3>Quantity: {offer.quantity}</h3>
       <h3>Gardener: </h3>
+      <button className='offer_delete-button'>delete offer</button>
+      <button className='offer_delete-button'>edit offer</button>
     </div>
   );
 }
